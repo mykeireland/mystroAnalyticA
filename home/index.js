a1b638246ee35540ab91ff3427d11f976e115c82
mykeireland <152150214+mykeireland@users.noreply.github.com>
2025-08-15 11:49:15 +1000

    Update index.js

--- home/index.js
+++ home/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
   context.res = {
     status: 302,
     headers: {
-      Location: "/static/index.html"
+      Location: "/static/index/"
     }
   };
 };
